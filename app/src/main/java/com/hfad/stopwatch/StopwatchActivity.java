10f5233e1abe887467250c1d9fb426e4aa68e288
tyler <tylerkecraine@gmail.com>
2020-09-11 13:15:42 -0400

    welp

---
+++ app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -0,0 +1,18 @@
+package com.hfad.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class StopwatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
+
+    public void onClickStart(View view) {
+    }
+}
